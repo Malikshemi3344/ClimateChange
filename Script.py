c17e130658746ed2b78f90941dd19f55442b5862
Malikshemi3344 <126503920+Malikshemi3344@users.noreply.github.com>
2023-03-24 06:27:58 +0500

    Added Methane per Person analysis.

--- Script.py
+++ Script.py
@@ -7,6 +7,9 @@ import os
 chosenCountries = ['China', 'Canada', 'India', 'Pakistan',
                    'United Kingdom', 'Australia', 'United States', 'Nigeria']
 
+# font style for figures
+font = dict(size=25)
+
 
 def getDataframes(filename):
     '''
@@ -43,27 +46,32 @@ def selectYears(yearsList, div=5):
     return [year for year in yearsList if not (int(year) % div)]
 
 
+# read all the files
 yearGDP, countryGDP = getDataframes('./Data/GDPPerCapita$.csv')
 yearCO2, countryCO2 = getDataframes('./Data/CO2EmissionsTonPerCapita.csv')
 yearMethane, countryMethane = getDataframes('./Data/MethaneEmissionTotal.csv')
 yearPopu, countryPopu = getDataframes('./Data/PopulationTotal.csv')
 yearPower, countryPower = getDataframes(
     './Data/PowerConsumptionkWhPerCapita.csv')
-yearForest, countryForest = getDataframes('./Data/ForestArea%.csv')
+yearForest, countryForest = getDataframes('./Data/ForestArea.csv')
 yearArable, countryArable = getDataframes('./Data/ArableLand%.csv')
 yearAgri, countryAgri = getDataframes('./Data/AgriculturalLand%.csv')
 
 
 # GDP per CO2 Emission
 
+# find common years between the two dataframes and select the years divisble by 5
 selectedYears = selectYears(getCommonYears(yearCO2.columns, yearGDP.columns))
+
+# get GDP per CO2 Emission table
 GDPPerCO2 = yearGDP[selectedYears] / yearCO2[selectedYears]
 
 fig = plt.figure(figsize=(20, 20))
+
+# plot line for each country
 for country in chosenCountries:
     ax = plt.plot(GDPPerCO2.loc[country], label=country)
 
-font = dict(size=25)
 plt.legend(fontsize='xx-large')
 plt.title('Energy Efficiency', fontdict=font)
 plt.xticks(fontsize=20)
@@ -71,3 +79,27 @@ plt.xlabel('Year', fontdict=font)
 plt.yticks(fontsize=20)
 plt.ylabel('GDP ($) / CO2 Emission (Ton)', fontdict=font)
 plt.savefig('EneryEfficiency')
+
+
+# Methane per Capita
+
+# find common years between the two dataframes and select the years divisble by 5
+selectedYears = selectYears(getCommonYears(yearMethane, yearPopu))
+
+# get Methane Emission per Person
+MethanePerCapita = yearMethane[selectedYears] / yearPopu[selectedYears]
+
+# melt the dataframe so that the years are under a single column
+df = MethanePerCapita.reset_index().melt(id_vars='Country Name', var_name='Year',
+                                         value_name='Methane per Person')
+
+# plot a barplot with countries on the x-axis and
+plt.figure(figsize=(20, 20))
+sns.barplot(x='Country Name', y='Methane per Person', hue='Year', data=df)
+plt.legend(fontsize='xx-large')
+plt.title('Methane Emissions per Capita', fontdict=font)
+plt.xticks(fontsize=20)  # increase font size of x-axis labels to 20
+plt.xlabel('Year', fontdict=font)
+plt.yticks(fontsize=20)  # increase font size of y-axis labels to 20
+plt.ylabel('Methane (Tons) / Person', fontdict=font)
+plt.savefig('MethanePerCapita')
